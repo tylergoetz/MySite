d0edcffee401a3372b2644e0d6ab98c227d0513b
Tyler Goetz <goetztyler813@gmail.com>
2019-04-04 08:23:27 -0500

    Add files via upload

---
+++ ps1.js
@@ -0,0 +1,8 @@
+'use strict'
+//js for ps1
+
+window.onload = function(){
+  $("#jumbo").fadeOut(0);
+  $("#jumbo").fadeIn(1500);
+
+}
---
+++ ps_blog.js
@@ -0,0 +1,9 @@
+//ps_blog.html > js
+'use strict'
+//js for ps1
+
+window.onload = function(){
+  $("#jumbo").fadeOut(0);
+  $("#jumbo").fadeIn(1500);
+
+}
